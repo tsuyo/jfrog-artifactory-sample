7fd33570bd2039a1ded769638b32d273dc1bd912
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2022-02-13 18:41:56 +0900

    fix go build.sh

--- go/build.sh
+++ go/build.sh
@@ -3,7 +3,7 @@ build_name=$2
 build_number=$3
 version=$4
 
-[ -z "${project}" -o -z "${build_name}" -o -z "${build_number}" -o -z "${version}" ] && echo "Usage: ${FUNCNAME[0]} <project> <build_name> <build_number> <version>" && exit 1
+[ -z "${project}" -o -z "${build_name}" -o -z "${build_number}" -o -z "${version}" ] && echo "Usage: $(basename $0) <project> <build_name> <build_number> <version>" && exit 1
 
 jfrog go build --project=${project} --build-name=${build_name} --build-number=${build_number}
 jfrog gp ${version} --project=${project} --build-name=${build_name} --build-number=${build_number}
